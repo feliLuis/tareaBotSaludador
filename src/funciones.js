408e47d841d77903d0a2d590d5774abeab22c48e
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-02-15 23:45:36 -0400

    feat: agregamos la funcionalidad de generar el saludo

--- src/funciones.js
+++ src/funciones.js
@@ -4,3 +4,9 @@ function obtenerSaludoPorHora() {
   if (hora < 18) return "Buenas tardes";
   return "Buenas noches";
 }
+
+export function generarSaludo(nombre, genero, edad, idioma) {
+  let saludo = obtenerSaludoPorHora(idioma);
+  let tratamiento = obtenerTratamiento(genero, edad, idioma);
+  return `${saludo} ${tratamiento} ${nombre}`.trim();
+}
