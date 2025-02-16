081703e770289d853876e471c4ca09a2e7e7769b
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-02-15 23:27:20 -0400

    feat:agregamos funcion para saber si es de dia o de noches o medio dia

---
+++ src/funciones.js
@@ -0,0 +1,6 @@
+function obtenerSaludoPorHora() {
+  const hora = new Date().getHours();
+  if (hora < 12) return "Buenos días";
+  if (hora < 18) return "Buenas tardes";
+  return "Buenas noches";
+}
