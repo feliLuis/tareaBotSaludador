b2d3d5dabc0b7359c3324fd95dd9f767670d7c25
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-02-15 23:59:51 -0400

    feat: agregamos la funcionalidad para el idioma y corregimos errores

--- src/funciones.js
+++ src/funciones.js
@@ -1,8 +1,25 @@
-function obtenerSaludoPorHora() {
+export function obtenerSaludoPorHora(idioma) {
   const hora = new Date().getHours();
-  if (hora < 12) return "Buenos días";
-  if (hora < 18) return "Buenas tardes";
-  return "Buenas noches";
+  if (idioma === "en") {
+      if (hora < 12) return "Good morning";
+      if (hora < 18) return "Good afternoon";
+      return "Good evening";
+  } else {
+      if (hora < 12) return "Buenos días";
+      if (hora < 18) return "Buenas tardes";
+      return "Buenas noches";
+  }
+}
+
+export function obtenerTratamiento(genero, edad, idioma) {
+  if (edad > 30) {
+      if (idioma === "en") {
+          return genero === "M" ? "Mr." : "Mrs.";
+      } else {
+          return genero === "M" ? "Sr." : "Sra.";
+      }
+  }
+  return "";
 }
 
 export function generarSaludo(nombre, genero, edad, idioma) {
